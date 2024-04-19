f2ad6c186c7039a3e78eda0c4b41ecaf1e8a8fde
liuyang <liuyang141@lenovo.com>
2024-04-19 18:37:12 +0800

    edit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,28 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import lodash from 'lodash';
+import {useCallback} from 'react'
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useCallback(
+    lodash.debounce((value) => {
+      console.log(value,'valuevaluevalue');
+        const params = new URLSearchParams(searchParams);
+        if(value?.target?.value) {
+            params.set('query', value?.target?.value);
+          } else {
+            params.delete('query');
+        }
+        replace(`${pathname}?${params.toString()}`);
+    }, 1000),[])
+  
+
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +31,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={handleSearch}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
